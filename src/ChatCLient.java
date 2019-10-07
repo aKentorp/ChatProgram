71fb0ac2b35769bb3616a82d43b9f89fd6d01af4
aKentorp <x4rn4x@gmail.com>
2019-10-07 11:21:22 +0200

    Nu med 1 bruger'ish

--- src/ChatCLient.java
+++ src/ChatCLient.java
@@ -3,6 +3,8 @@ import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class ChatCLient {
 
@@ -12,16 +14,60 @@ public class ChatCLient {
         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
         DatagramSocket clientSocket = new DatagramSocket();
         InetAddress IPAddress = InetAddress.getByName("localhost");
+        int port = 9876;
+        String[] errorTest;
+
+        System.out.println();
 
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
+        while(true){
+            String userName = userInput.readLine();
+            sendData = ("JOIN " + userName + ", " + IPAddress + ":" + port).getBytes();
+            System.out.println("1");
+            if (userName.length() < 12 && stringContainsNumber(userName) == false && !userName.contains(" ")){
+                System.out.println("2");
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                clientSocket.send(sendPacket);
 
-        String initialHandshake = (IPAddress.toString());
-        sendData = initialHandshake.getBytes();
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
 
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+                errorTest = new String(receivePacket.getData()).split("\\W", 2);
+                System.out.println("3");
+
+                if(errorTest[0].equalsIgnoreCase("J_OK")){
+
+                    System.out.println(errorTest[0]);
+
+                    while(true){
+                        System.out.println("4");
+                        Arrays.fill(sendData, (byte)0);
+                        sendData = ("DATA " + userName + ": " + userInput.readLine()).getBytes();
+                        sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                        clientSocket.send(sendPacket);
+
+                        Arrays.fill(receiveData, (byte)0);
+                        receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                        clientSocket.receive(receivePacket);
+                        System.out.println(new String(receivePacket.getData()));
 
 
+                    }
 
+                }
+                else{
+                    System.out.println(errorTest[0]);
+                    System.out.println("5");
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    public static boolean stringContainsNumber( String s )
+    {
+        return Pattern.compile( "[0-9]" ).matcher( s ).find();
     }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,5 +1,9 @@
+import javax.xml.crypto.Data;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ChatServer {
 
@@ -9,8 +13,51 @@ public class ChatServer {
         byte[] receiveData = new byte[1024];
         byte[] sendData = new byte[1024];
         ArrayList<String> userNames = new ArrayList<>();
+        String tempUserName;
+        String tempMessage;
+        boolean error = false;
 
         while(true){
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String[] receivedDataString = new String(receivePacket.getData()).split("\\W");
+            tempUserName = receivedDataString[1];
+            error = false;
+
+            for(String userName : userNames){
+                if(userName.equalsIgnoreCase(tempUserName)){
+                    error=true;
+                    sendData = ("J_ER 1: USER NAME TAKEN").getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+                    serverSocket.send(sendPacket);
+                }
+            }
+            if(error != true){
+                userNames.add(tempUserName);
+                sendData = ("J_OK").getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+                serverSocket.send(sendPacket);
+                while (true) {
+                    Arrays.fill(receiveData, (byte)0);
+                    receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    serverSocket.receive(receivePacket);
+                    System.out.println(new String(receivePacket.getData()));
+
+
+                    Arrays.fill(sendData, (byte)0);
+                    sendData = receiveData;
+                    sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+                    serverSocket.send(sendPacket);
+
+
+
+                }
+
+            }
+
+
+
 
 
         }
